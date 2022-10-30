7acb98cfa6c3d3247e7091d485f3ce8989dbe03d
Ose Ogbe <therealnylat@gmail.com>
2022-10-30 14:38:33 +0100

    hero and navmenu mobile updates

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,16 +1,33 @@
 <template>
-  <div class="relative h-screen bg-[url('@/assets/img/aybee-adayi.jpg')] bg-cover bg-no-repeat grayscale">
+  <div class="relative h-screen bg-[url('@/assets/img/aybee-adayi.jpg')] bg-cover bg-center bg-no-repeat grayscale">
     <slot />
-    <div class="h-screen absolute px-8">
-      <div class="line h-2/6 w-0.5 ml-6 my-6 bg-black"></div>
-      <div class="flex flex-col gap-y-6 ml-3">
-        <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'facebook']"/>
-        <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'twitter']"/>
-        <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'instagram']"/>
+    <div class="h-screen absolute px-2 md:px-8">
+      <div class="h-3/6 md:h-2/6 w-0.5 ml-6 my-6 bg-transparent md:bg-black"></div>
+      <div class="hidden md:flex flex-col gap-y-6 ml-3">
+        <a href="https://web.facebook.com/abraham.abel.752/" target="_blank">
+          <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'facebook']"/>
+        </a>
+        <a style="margin-left: 2px;" href="https://instagram.com/aybee_adayi/" target="_blank">
+          <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'instagram']"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UC4bO-Zg_yqPOdyKU_K2ZBVA/" target="_blank">
+          <font-awesome-icon font-size="24" color="#000" :icon="['fab', 'youtube']"/>
+        </a>
       </div>
-      <div class="line h-2/6 w-0.5 ml-6 mt-6 bg-black"></div>
+      <div class="flex flex-col md:hidden gap-y-6 ml-4">
+        <a href="https://web.facebook.com/abraham.abel.752/" target="_blank">
+          <font-awesome-icon font-size="16" color="#fff" :icon="['fab', 'facebook']"/>
+        </a>
+        <a style="margin-left: 1px;" href="https://instagram.com/aybee_adayi/" target="_blank">
+          <font-awesome-icon font-size="16" color="#fff" :icon="['fab', 'instagram']"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UC4bO-Zg_yqPOdyKU_K2ZBVA/" target="_blank">
+          <font-awesome-icon font-size="16" color="#fff" :icon="['fab', 'youtube']"/>
+        </a>
+      </div>
+      <div class="h-1/6 md:h-2/6 w-0.5 ml-6 mt-6 bg-transparent md:bg-black"></div>
     </div>
-    <div class="w-100 absolute top-2/4 left-1/4 -translate-x-2/4 -translate-y-1/4 px-24">
+    <div class="hidden md:block w-100 absolute top-2/4 left-1/4 -translate-x-2/4 -translate-y-1/4 px-24">
       <h1 class="text-white text-8xl font-bold mb-6">
         <span class="px-4 py-1" style="background-color: #70707080;">
           Aybee Adayi
@@ -25,6 +42,23 @@
         :backDelay='70'
       >
         <p class="text-black text-5xl font-medium pl-4"><span class="typing"></span></p>
+      </vue-typed-js>
+    </div>
+    <div class="md:hidden w-100 absolute top-3/4 right-0 -translate-y-3/4">
+      <h1 class="text-white text-5xl font-bold mb-6">
+        <span class="px-4 py-1" style="background-color: #70707080;">
+          Aybee Adayi
+        </span>
+      </h1>
+      <vue-typed-js
+        :strings="['Creative Artist', 'Photographer', 'Videographer', 'Graphics Designer']"
+        :loop="true"
+        :smartBackspace='true'
+        :typeSpeed='120'
+        :backSpeed='50'
+        :backDelay='70'
+      >
+        <p class="text-white text-2xl font-medium pl-4"><span class="typing"></span></p>
       </vue-typed-js>
     </div>
   </div>
--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="w-full flex items-center justify-between py-4 px-12">
+  <div class="w-full flex items-center justify-between py-4 px-6 md:px-12">
     <span class="w-24 md:w-auto">
-      <h1 class="text-2xl font-semibold">Aybee
-        <span class="bg-black pl-4 pr-4">
+      <h1 class="text-1xl md:text-2xl font-semibold">Aybee
+        <span class="bg-black pl-1 md:pl-4 pr-1 md:pr-4">
           <span class="text-slate-100">Adayi</span>
         </span>
       </h1>
     </span>
-    <ul class="flex">
+    <ul class="hidden md:flex">
       <li class="ml-12">
         <a href="#hero" class="text-lg font-medium">Home</a>
       </li>
@@ -27,6 +27,13 @@
         <a href="#contact" class="text-lg font-medium">Contact</a>
       </li>
     </ul>
+    <div class="md:hidden">
+      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="32px" height="32px" color="#000" viewBox="32 32 384.97 384.97" xml:space="preserve">
+        <path d="M12.03,120.303h360.909c6.641,0,12.03-5.39,12.03-12.03c0-6.641-5.39-12.03-12.03-12.03H12.03c-6.641,0-12.03,5.39-12.03,12.03C0,114.913,5.39,120.303,12.03,120.303z"></path>
+        <path d="M372.939,180.455H12.03c-6.641,0-12.03,5.39-12.03,12.03s5.39,12.03,12.03,12.03h360.909c6.641,0,12.03-5.39,12.03-12.03 S379.58,180.455,372.939,180.455z"></path>
+        <path d="M372.939,264.667H132.333c-6.641,0-12.03,5.39-12.03,12.03c0,6.641,5.39,12.03,12.03,12.03h240.606 c6.641,0,12.03-5.39,12.03-12.03C384.97,270.056,379.58,264.667,372.939,264.667z"></path>
+      </svg>
+    </div>
   </div>
 </template>
 <script>
